21784a2e8c3bbef19def31e55eb5e00fe4e2dc48
CatLearned <vovanvinni@gmail.com>
2019-05-16 00:34:07 +0300

    JSON works! We can cmp versions and prog names!

--- updater.py
+++ updater.py
@@ -7,17 +7,14 @@ def check_for_updates():
 
     with open(path_json) as json_file:
         json_data = json.load(json_file)
-    print(json_data)
     p_name = json_data['Program name']
     p_vers = json_data['Version']
     print("Program name is", p_name, "\nVersion is", p_vers)
+
     response = urllib2.urlopen(path_remote)
     html = response.read()
-    print(html)
-    #print(html)
-    #with html as json_file:
-    json_data = json.dumps(html)
-    print(json_data)
+    json_string = html.decode()
+    json_data = json.loads(json_string)
     a_name = json_data['Program name']
     a_vers = json_data['Version']
     print("Actual name is", a_name, "\nVersion is", a_vers)
